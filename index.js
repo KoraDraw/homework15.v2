9435f8818619cc44009042a307b6d43b7e028f0f
KoraDraw <korinachernysheva@mail.ru>
2025-03-18 17:14:46 +0300

    create new homework

---
+++ index.js
@@ -0,0 +1,17 @@
+const taskInput = document.getElementById('task');
+        const buttonAdd = document.getElementById('buttonAdd');
+        const taskList = document.getElementById('taskList');
+        function createTask() {
+            const taskTextValue = taskInput.value.trim();
+            const taskItem = document.createElement('li');
+            taskItem.textContent = taskTextValue;
+            taskList.append(taskItem);
+            taskInput.value = '';
+        }
+        function checkTask() {
+	if (event.target.tagName === 'LI') {
+		event.target.classList.toggle('completed');
+	}
+};
+        buttonAdd.addEventListener('click', createTask);
+        taskList.addEventListener('click', checkTask);
