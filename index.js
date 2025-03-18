42c0287f207712c3be2c31a633cfd4ce5d959e59
KoraDraw <korinachernysheva@mail.ru>
2025-03-18 21:58:54 +0300

    add changes

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const taskInput = document.getElementById('task');
         const buttonAdd = document.getElementById('buttonAdd');
         const taskList = document.getElementById('taskList');
         function createTask() {
-            const taskTextValue = taskInput.value.trim();
-            const taskItem = document.createElement('li');
-            taskItem.textContent = taskTextValue;
-            taskList.append(taskItem);
-            taskInput.value = '';
+                const taskTextValue = taskInput.value.trim();
+                const taskItem = document.createElement('li');
+                taskItem.textContent = taskTextValue;
+                taskList.append(taskItem);
+                taskInput.value = '';
         }
-        function checkTask() {
-	if (event.target.tagName === 'LI') {
-		event.target.classList.toggle('completed');
-	}
-};
+
+        function checkTask(event) {
+                if (event.target.tagName === 'LI') {
+                event.target.classList.toggle('completed');
+                }
+        }
         buttonAdd.addEventListener('click', createTask);
         taskList.addEventListener('click', checkTask);
